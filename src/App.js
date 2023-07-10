54a266bf9963d60da66f6d7d6d968dd08ad80ad6
Lee_giun <sunday6602@naver.com>
2023-07-10 12:33:57 +0900

    '05_0710'

--- src/App.js
+++ src/App.js
@@ -39,7 +39,8 @@ function App() {
       <Routes>
         <Route path='/shop' element = {
           <Container>
-            <img src={process.env.PUBLIC_URL+'/images/visual.jpg'} alt='vm'/>
+            <img src='https://leegiun.github.io/Shop/images/visual.jpg' alt='vm1'/>
+            <img src={process.env.PUBLIC_URL+'https://leegiun.github.io/Shop/images/visual.jpg'} alt='vm2'/>
             <h2>Product</h2>
             <Row>
               {
